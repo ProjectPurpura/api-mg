591689b7baa57f6c2575c9bcbbaaf22abae874e2
Stuarte.exe <157625546+EmilioStuart@users.noreply.github.com>
2025-09-07 13:57:15 -0300

    (feat): Implementa a inicialização do Dotenv para carregar o arquivo .env

--- src/main/java/org/purpura/apimg/ApiMgApplication.java
+++ src/main/java/org/purpura/apimg/ApiMgApplication.java
@@ -1,13 +1,14 @@
 package org.purpura.apimg;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiMgApplication {
-
+    // Carrega as variáveis de ambiente do arquivo .env
+    Dotenv dotenv = Dotenv.load();
     public static void main(String[] args) {
         SpringApplication.run(ApiMgApplication.class, args);
     }
-
 }
