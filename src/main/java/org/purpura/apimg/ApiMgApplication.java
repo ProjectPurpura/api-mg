e034d31a7b5aabf274df4c89a593e3bd06b478e6
Stuarte.exe <157625546+EmilioStuart@users.noreply.github.com>
2025-09-07 15:06:05 -0300

    (chore): Remove dependência dotenv-java por ser redundante com plugin da IDE

--- src/main/java/org/purpura/apimg/ApiMgApplication.java
+++ src/main/java/org/purpura/apimg/ApiMgApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiMgApplication {
     public static void main(String[] args) {
-        // Carrega as variáveis de ambiente do arquivo .env
-        Dotenv.load();
-
         SpringApplication.run(ApiMgApplication.class, args);
     }
 }
