3a70536ebbb825f6ed119ba37ca6c39a57131a41
Stuarte.exe <157625546+EmilioStuart@users.noreply.github.com>
2025-09-07 15:23:24 -0300

    (bugfix): Remoção do import no ApiMgApplication.java

--- src/main/java/org/purpura/apimg/ApiMgApplication.java
+++ src/main/java/org/purpura/apimg/ApiMgApplication.java
@@ -1,6 +1,5 @@
 package org.purpura.apimg;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
