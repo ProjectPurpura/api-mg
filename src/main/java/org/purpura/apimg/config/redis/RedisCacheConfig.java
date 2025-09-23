00172368fff608e5b3b3dfde11af9722d8a6bf94
rkhue <85773727+rkhue@users.noreply.github.com>
2025-09-23 08:38:22 -0300

    (feat) Configurando serialização JSON para valores de cache com Jackson2JsonRedisSerializer

--- src/main/java/org/purpura/apimg/config/redis/RedisCacheConfig.java
+++ src/main/java/org/purpura/apimg/config/redis/RedisCacheConfig.java
@@ -1,10 +1,14 @@
 package org.purpura.apimg.config.redis;
 
+import org.purpura.apimg.dto.empresa.base.EmpresaResponseDTO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 import java.time.Duration;
 
@@ -15,7 +19,10 @@ public class RedisCacheConfig {
     public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
         RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10))
-                .disableCachingNullValues();
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                        new Jackson2JsonRedisSerializer<>(EmpresaResponseDTO.class)
+                ));
 //                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
 //                        new Jackson2JsonRedisSerializer<>(CepResponseDTO.class)));
 
